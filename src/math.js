cd8184fc0b3f2349faf57f2b10498b0bc7e8c3b4
Owen Hardy <owen@owens-mbp.mynetworksettings.com>
2023-02-17 16:50:49 -0600

    math tests

---
+++ src/math.js
@@ -0,0 +1,3 @@
+const calculateTip (total, tipPercent) => {
+    const tip = total * tipPercent
+}
---
+++ tests/math.test.js
@@ -0,0 +1,7 @@
+test('Hello world', () => {
+
+})
+
+test('This test should fail', () => {
+    throw new Error('Failure!')
+})
