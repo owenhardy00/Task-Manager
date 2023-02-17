e1c4bced5fe2f4f54f6553e2ca6c86e6bf6c4839
Owen Hardy <owen@owens-mbp.mynetworksettings.com>
2023-02-17 17:26:18 -0600

    more tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,22 @@
-test('Hello world', () => {
+const { calculateTip, fahrenheitToCelcius, celciusToFahrenheit } = require('../src/math')
 
+test('Should calculate total with tip', () => {
+    expect(calculateTip(10, 0.3)).toBe(13)
+
+    // if (calculateTip(10, 0.3) !== 13) {
+    //     throw new Error(`Total tip should be $13, got $${total} `)
+    // }
 })
 
-test('This test should fail', () => {
-    throw new Error('Failure!')
+test('Should calculate total with default tip', () => {
+    expect(calculateTip(10)).toBe(12.5)
 })
+
+test('Should convert 32 F to 0 C', () => {
+    expect(fahrenheitToCelcius(32)).toBe(0)
+})
+
+test('Should convert 0 C to 32 F', () => {
+    expect(celciusToFahrenheit(0)).toBe(32)
+})
+
